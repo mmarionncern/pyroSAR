6b3f65608ed847375e76f00656739decf956d836
mmarionncern <matmarionneau@gmail.com>
2025-04-17 14:13:32 +0200

    Remove debug prints

--- pyroSAR/snap/util.py
+++ pyroSAR/snap/util.py
@@ -978,7 +978,6 @@ def halpha(infile, swaths=["IW1", "IW2", "IW3"], t_srs=4326, demName='SRTM 1Sec 
         tmpdir = outdir
 
     for iw in swaths:
-        print("HALPHA -> ", iw)
         # parse base workflow
         workflow = parse_recipe('blank')
         ############################################
@@ -1044,7 +1043,6 @@ def halpha(infile, swaths=["IW1", "IW2", "IW3"], t_srs=4326, demName='SRTM 1Sec 
 
     import glob
     tmp_c2_files = glob.glob(f"{tmpdir}/S1_HAlpha_*_{date}_tmp.dim")
-    print(tmp_c2_files)
 
     workflow = parse_recipe('blank')
     reads = []
@@ -1228,35 +1226,33 @@ def insar_coherence(infiles, swaths=["IW1", "IW2", "IW3"], polarizations='all', 
 
     if not isinstance(infiles, list):
         raise RuntimeError("'infiles' must be of type list")
-    print("bli 1")
     if len(infiles) != 2:
         raise RuntimeError("Two files are mandatory to compute Insar coherence, less or more were provided")
-    print("bli 2")
+
     ids = identify_many(infiles, sortkey='start')
-    print("bli 3")
     for id in ids:
         log.info(id.meta)
     id_1 = ids[0]
     id_2 = ids[1]
-    print("bli 4")
+
     if id_1.is_processed(outdir):
         log.info(f'scene {id_1.outname_base()} already processed')
         return
 
     if not os.path.isdir(outdir):
         os.makedirs(outdir)
-    print("bli 5")
+
     ############################################
     # general setup
     if id_1.sensor not in ['S1A', 'S1B', 'S1C'] or id_1.product != "SLC" or\
        id_2.sensor not in ['S1A', 'S1B', 'S1C'] or id_2.product != "SLC":
         raise RuntimeError('Insar coherence only available for Sentinel mission in SLC mode')
-    print("bli 6")
+
     formatName = 'SENTINEL-1'
 
     if id_1.acquisition_mode != 'IW' or id_2.acquisition_mode != 'IW':
         raise RuntimeError(f"acquisition mode {id_1.acquisition_mode}/{id_2.acquisition_mode} not supported")
-    print("bli 7")
+
     # check s1 frame compatibility
     # second image has to be the first image used for coherence estimation
     delta_t = (dt.datetime.strptime(id_2.start, "%Y%m%dT%H%M%S") -
@@ -1275,7 +1271,7 @@ def insar_coherence(infiles, swaths=["IW1", "IW2", "IW3"], polarizations='all', 
         logger.error(delta_t)
         raise RuntimeError("Invalid S1 frame comparison, wrong geographical matching")
     ######################
-    print("bli 8")
+
     if isinstance(polarizations, str):
         if polarizations == 'all':
             polarizations = id_1.polarizations
@@ -1288,18 +1284,17 @@ def insar_coherence(infiles, swaths=["IW1", "IW2", "IW3"], polarizations='all', 
         polarizations = [x for x in polarizations if x in id.polarizations]
     else:
         raise RuntimeError('polarizations must be of type str or list')
-    print("bli 9")
+
     if externalDEMFile is None and externalDEMNoDataValue is None:
         externalDEMNoDataValue = 0.
-    print("bli 10")
+
     if tmpdir is None:
         tmpdir = outdir
 
     ############################################
-    print("bli 11", polarizations, swaths)
+
     for pol in polarizations:
         for iw in swaths:
-            print("coherence -> ", iw, pol)
             # parse base workflow
             workflow = parse_recipe('blank')
             ############################################
@@ -1393,7 +1388,6 @@ def insar_coherence(infiles, swaths=["IW1", "IW2", "IW3"], polarizations='all', 
         # back to all iws, single polarization
         import glob
         tmp_coh_files = glob.glob(f"{tmpdir}/S1_*_COH_{pol}_{date1}_{date2}_tmp.dim")
-        print(tmp_coh_files)
 
         workflow = parse_recipe('blank')
         reads = []
