ad53fa9b97ad27e609d3d14be026832b6e6a350d
mmarionncern <matmarionneau@gmail.com>
2026-06-23 16:42:38 +0200

    add S1C/D

--- pyroSAR/snap/util.py
+++ pyroSAR/snap/util.py
@@ -273,7 +273,7 @@ def geocode(infile, outdir, t_srs=4326, spacing=20, polarizations='all', shapefi
 
     if id.sensor in ['ASAR', 'ERS1', 'ERS2']:
         formatName = 'ENVISAT'
-    elif id.sensor in ['S1A', 'S1B']:
+    elif id.sensor in ['S1A', 'S1B', "S1C", "S1D"]:
         if id.product == 'SLC':
             removeS1BorderNoise = False
             process_S1_SLC = True
